034072f72ff07003acd0f414c9a9ee23e7279eea
Manish4567jkl <manishmandal895@gmail.com>
2023-12-23 16:27:33 +0530

    add styling

--- src/App.jsx
+++ src/App.jsx
@@ -27,9 +27,49 @@ setPassword(pass);
 
 },[length,numberAllowed,specialCharacterAllowed,setPassword])
 
+
   return (
   <>
-    <h1 className='text-center text-7xl text-indigo-400 font-bold'>Password Generator</h1>
+   <div className='bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-slate-900 via-purple-900 to-slate-900 h-[100vh] w-[100vw] flex justify-center items-center flex-col'>
+   <h1 className='text-center text-7xl text-indigo-400 font-bold'>Password Generator</h1>
+    <div className='flex justify-center items-center h-2/6 w-3/6 '>
+    <input 
+    className='w-[500px] h-[70px] outline-none p-5  shadow-lg'
+   type="text"
+   value={password}
+   readOnly
+   placeholder='Password'
+   />
+   <button className='h-[70px] w-[130px] bg-slate-400 shadow-lg'>
+    Copy
+   </button>
+    </div>
+    <div className='text-white font-mono text-2xl'>
+      <input type="range" 
+      min={8}
+      max={32}
+      value={length}
+      onChange={(e) => {setLength(e.target.value)}}
+      className='cursor-pointer'
+      />
+      <label className='mx-3 font-mono text-2xl'>Length:{length}</label>
+      <input type="checkbox" 
+      defaultChecked={numberAllowed}
+      onChange={()=> {
+        setNumberAllowed((prev) => !prev)
+      }}
+      />
+      <label className='mx-3 '>Numbers</label>
+
+      <input type="checkbox" 
+      defaultChecked={specialCharacterAllowed}
+      onChange={() => {
+        setSpecialCharacterAllowed((prev) => !prev)
+      }}
+/>
+      <label className='mx-3'>Special-Characters</label>
+    </div>
+   </div>
   </>
   )
 }
